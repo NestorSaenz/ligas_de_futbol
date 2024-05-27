4be966a0e1fbaa1db4dbcbc93ed4dea50a784290
Nestor Saenz <nes164@gmail.com>
2024-05-26 22:28:40 -0500

    Moviendo archivos a la carpeta airflow

--- Airflow_deploy/dags/demo_leagues/querys/upload_table.sql
+++ Airflow_deploy/dags/demo_leagues/querys/upload_table.sql
@@ -1,1 +1,3 @@
-copy into {{params.table}} from @{{params.stage }}/premier_positions.csv.gz FILE_FORMAT=(TYPE= CSV field_delimeter=',' skip_header=1) ON_ERROR = 'CONTINUE';
+
+use warehouse NORMAL_WH;
+copy into {{ params.table }} from @{{ params.stage }}/premier_positions.csv.gz FILE_FORMAT=(TYPE=csv field_delimIter=',' skip_header=1) ON_ERROR = 'CONTINUE';
